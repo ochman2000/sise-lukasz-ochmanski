e680ee7c75e01fa654e305cd9f082d8652d60221
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-04-09 23:21:06 +0000

    To jest coś

--- trunk/01/src/Driver.java
+++ trunk/01/src/Driver.java
@@ -1,9 +1,12 @@
+import pl.lodz.p.sise.Puzzle;
+
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] a = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
+		Puzzle p = new Puzzle(a);
+		System.out.println(p.getStringRepresentationi());
+		System.out.println(p);
 	}
-
 }
