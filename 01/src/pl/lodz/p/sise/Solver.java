f7da2965654c3a3bb8e285032fbd3a803da71436
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-04-10 23:23:40 +0000

    DFS i BFS działają.

--- 01/src/pl/lodz/p/sise/Solver.java
+++ 01/src/pl/lodz/p/sise/Solver.java
@@ -1,16 +1,25 @@
 package pl.lodz.p.sise;
 
-import java.util.List;
-
+import pl.lodz.p.sise.algorithms.BFS;
 import pl.lodz.p.sise.algorithms.DFS;
 
 public class Solver {
 
 	public static void main(String[] args) {
+		
+		System.out.println("=========================================================");
+		System.out.println("DEPTH FIRST SEARCH: ");
+		System.out.println("=========================================================");
+		System.out.println();
 		DFS dfs = new DFS();
-		List<Ruch> list = dfs.search();
-		for (Ruch ruch : list) {
-			System.out.print(ruch+",");
-		}		
+		System.out.println(dfs.search());
+		
+		System.out.println("=========================================================");
+		System.out.println("BREADTH FIRST SEARCH: ");
+		System.out.println("=========================================================");
+		System.out.println();
+		BFS bfs = new BFS();
+		System.out.println(bfs.search());
+		
 	}
 }
