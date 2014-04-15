5850cb516efec113c6ca54a6045edcd1e225f03e
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-04-15 21:31:42 +0000

    Struktura do testów jest.

--- trunk/01/src/pl/lodz/p/sise/structure/Edge.java
+++ trunk/01/src/pl/lodz/p/sise/structure/Edge.java
@@ -1,12 +1,21 @@
 package pl.lodz.p.sise.structure;
 
+import pl.lodz.p.sise.Puzzle;
+
 public class Edge {
 	private final String id;
-	private final Vertex source;
-	private final Vertex destination;
+	private final Puzzle source;
+	private final Puzzle destination;
 	private final int weight;
 
-	public Edge(String id, Vertex source, Vertex destination, int weight) {
+	/**
+	 * 
+	 * @param id - nazwa
+	 * @param source - koniec A
+	 * @param destination - koniec B
+	 * @param weight - odległość
+	 */
+	public Edge(String id, Puzzle source, Puzzle destination, int weight) {
 		this.id = id;
 		this.source = source;
 		this.destination = destination;
@@ -17,11 +26,11 @@ public class Edge {
 		return id;
 	}
 
-	public Vertex getDestination() {
+	public Puzzle getDestination() {
 		return destination;
 	}
 
-	public Vertex getSource() {
+	public Puzzle getSource() {
 		return source;
 	}
 
