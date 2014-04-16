382c272bf540fac055d2f8daa8f86b8b4eb018a6
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-04-16 22:28:57 +0000

    Dodana struktura grafu

--- 01/src/pl/lodz/p/sise/algorithm/Dijkstra.java
+++ 01/src/pl/lodz/p/sise/algorithm/Dijkstra.java
@@ -1,15 +1,22 @@
 package pl.lodz.p.sise.algorithm;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import pl.lodz.p.sise.Puzzle;
 import pl.lodz.p.sise.Ruch;
+import pl.lodz.p.sise.Visited;
 import pl.lodz.p.sise.exception.DuplicatelPuzzleException;
 import pl.lodz.p.sise.exception.IllegalPuzzleException;
 import pl.lodz.p.sise.exception.PuzzleFormatException;
 import pl.lodz.p.sise.structure.Edge;
+import pl.lodz.p.sise.structure.Fringe;
 import pl.lodz.p.sise.structure.Graph;
+import pl.lodz.p.sise.structure.Predecessor;
 
 public class Dijkstra {
 
@@ -47,8 +54,29 @@ public class Dijkstra {
 	}
 	
 	public List<Ruch> search() {
+		Fringe fringe = new Fringe(); 	//PREDECESSORS WITH THE DISTANCE
+		List<Ruch> result = new ArrayList<Ruch>();
+		Visited visited = new Visited();
+		Entry<Puzzle, Predecessor> currentNode = null;
 		
+		//DEFINIUJEMY MIEJSCE STARTU
+		Puzzle start = graph.getVertexes().get(0);
+		Puzzle b = graph.getVertexes().get(1);
+		fringe.put(b, new Predecessor(1, start));
+		
+		while (visited.size() < graph.numberOfNodes()) {
+			if (currentNode.getKey().isSolved())
+				return result;
+			
+			//WYBIERZ NAJKRÓTSZĄ ŚCIEŻKĘ Z FRINGE'A
+			currentNode = fringe.getLowestCostPath();
+			//TERAZ ZNAJDŹ WSZYSTKICH SĄSIADÓW TEGO WĘZŁA
+			
+			
+			
+		}
 		
 		return null;
 	}
+	
 }
