b0be75b606fc2341673eb506b1137871448c3dc9
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-04-17 00:11:57 +0000

    Poprawiony bug null pointer exception

--- 01/src/pl/lodz/p/sise/structure/Fringe.java
+++ 01/src/pl/lodz/p/sise/structure/Fringe.java
@@ -7,7 +7,7 @@ import java.util.Map.Entry;
 
 import pl.lodz.p.sise.Puzzle;
 
-public class Fringe extends HashMap<Puzzle, Predecessor> {
+public class Fringe<V> extends HashMap<Puzzle, Predecessor> {
 
 	/**
 	 * 
@@ -18,7 +18,7 @@ public class Fringe extends HashMap<Puzzle, Predecessor> {
 		Map.Entry<Puzzle, Predecessor> ret = null;
 		int shortestDistance = Integer.MAX_VALUE;
 		for (Iterator<Entry<Puzzle, Predecessor>> iterator = this.entrySet().iterator(); iterator.hasNext();) {
-			java.util.Map.Entry<Puzzle, Predecessor> e = iterator.next();
+			Entry<Puzzle, Predecessor> e = iterator.next();
 			int vertexDistance = e.getValue().getDistance();
 			if (vertexDistance<shortestDistance) {
 				ret = e;
@@ -27,8 +27,13 @@ public class Fringe extends HashMap<Puzzle, Predecessor> {
 		return ret;
 	}
 	
-	public Predecessor get(Puzzle key) {
-		return this.get(key);
+	public Predecessor get(Object key) {
+		for (Iterator<Entry<Puzzle, Predecessor>> iterator = this.entrySet().iterator(); iterator.hasNext();) {
+			Entry<Puzzle, Predecessor> e = iterator.next();
+			if (e.getKey().equals(key))
+				return e.getValue();
+		}
+		return null;
 	}
-	
+
 }
