6c09ac4420f797ac2039c5493e4c4ed1c95802d3
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-04-17 21:26:55 +0000

    Algorytm napisany, choć jeszcze nie działa.

--- trunk/01/src/pl/lodz/p/sise/structure/Fringe.java
+++ trunk/01/src/pl/lodz/p/sise/structure/Fringe.java
@@ -2,27 +2,34 @@ package pl.lodz.p.sise.structure;
 
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import pl.lodz.p.sise.Puzzle;
+import pl.lodz.p.sise.Ruch;
+import pl.lodz.p.sise.Visited;
 
-public class Fringe<V> extends HashMap<Puzzle, Predecessor> {
+public class Fringe extends HashMap<Puzzle, Predecessor> {
 
+	private Visited visited;
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4814714233235085597L;
+	
+	public Fringe() {
+		visited = new Visited();
+	}
 
-	public Map.Entry<Puzzle, Predecessor> getLowestCostPath() {
-		Map.Entry<Puzzle, Predecessor> ret = null;
+	public Puzzle getLowestCostPath() {
+		Puzzle ret = null;
 		int shortestDistance = Integer.MAX_VALUE;
-		for (Iterator<Entry<Puzzle, Predecessor>> iterator = this.entrySet().iterator(); iterator.hasNext();) {
-			Entry<Puzzle, Predecessor> e = iterator.next();
+		Iterator<Entry<Puzzle, Predecessor>> it = this.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<Puzzle, Predecessor> e = it.next();
 			int vertexDistance = e.getValue().getDistance();
-			if (vertexDistance<shortestDistance) {
-				ret = e;
-			}
+			if (vertexDistance<shortestDistance && !visited.contains(e.getKey())) {
+				ret = e.getKey();
+			}	
 		}
 		return ret;
 	}
@@ -34,6 +41,22 @@ public class Fringe<V> extends HashMap<Puzzle, Predecessor> {
 				return e.getValue();
 		}
 		return null;
+	}
+	
+	/**
+	 * Wstawia nowy element.
+	 * @param a - węzeł do którego szuka się najkrótszej drogi
+	 * @param b - dotychczas uzyskana najkrótsza droga do a
+	 * @param c - węzeł, przez który trzeba było przejść, aby dostać się do a.
+	 * @param d - ruch, jaki doprowadził do tego węzła
+	 * <br/>
+	 * Na przykład: <br/>
+	 * fringe.put(b, 1, a, d);<br/>
+	 * Najkrótsza ścieżka do b przechodzi przez a i wynosi 1.
+	 * Ostatnio wykonano ruch d.
+	 */
+	public void put(Puzzle a, int b, Puzzle c, Ruch d) {
+		this.put(a, new Predecessor(b, c, d));
 	}
 
 }
