f2b6457877c42358df10f8f40ee0ded81f3c0721
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-06-11 14:27:29 +0000

    GUI menubar

--- 02/src/pl/lodz/p/sise/structure/Plansza.java
+++ 02/src/pl/lodz/p/sise/structure/Plansza.java
@@ -1,11 +1,17 @@
 package pl.lodz.p.sise.structure;
 
+import javafx.event.ActionEvent;
 import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -21,9 +27,15 @@ public class Plansza extends Application {
 	private Rectangle line01;
 	private Rectangle line02;
 	private Rectangle rect02;
+	private MenuItem playButton;
+	private MenuBar mainMenu;
+	private Menu fileMenu;
+	private MenuItem stopButton;
+	private MenuItem pauseButton;
 
 	@Override
 	public void start(Stage stage) throws Exception {
+
 		pane = new Pane();
 		pane.setId("pane");
 
@@ -33,6 +45,7 @@ public class Plansza extends Application {
 
 		rect01 = new Rectangle(100, 50);
 		rect01.setId("autko");
+		rect01.setY(225);
 
 		rect02 = new Rectangle(120, 70);
 		rect02.setId("miejsce");
@@ -53,7 +66,23 @@ public class Plansza extends Application {
 		line02.setX(700 - 7);
 		line02.setY(500 / 2 + (50 - 20) / 2 - 16);
 		line02.setRotate(60 + 90.0);
+		
+		playButton = new MenuItem("Play");
+		pauseButton = new MenuItem("Pause");
+		stopButton = new MenuItem("Stop");
 
+		fileMenu = new Menu("File");
+		fileMenu.getItems().add(playButton);
+		fileMenu.getItems().add(pauseButton);
+		fileMenu.getItems().add(stopButton);
+		fileMenu.getItems().add(new SeparatorMenuItem());
+		fileMenu.getItems().add(new MenuItem("Restart"));
+		fileMenu.getItems().add(new SeparatorMenuItem());
+		fileMenu.getItems().add(new MenuItem("Exit"));
+		
+		mainMenu = new MenuBar();
+		mainMenu.getMenus().add(fileMenu);
+		
 		translate = new TranslateTransition();
 		translate.setDuration(new Duration(5 * 1000));
 		translate.setNode(rect01);
@@ -71,22 +100,48 @@ public class Plansza extends Application {
 		rotate.setCycleCount(Timeline.INDEFINITE);
 		rotate.setInterpolator(Interpolator.EASE_BOTH);
 
-		pane.getChildren().addAll(rect01);
-		pane.getChildren().addAll(rect02);
-		pane.getChildren().addAll(line01);
-		pane.getChildren().addAll(line02);
-
+		
+		pane.getChildren().addAll(rect01, rect02, line01, line02, mainMenu);
+		
 		stage.setTitle("Sztuczna Inteligencja i Systemy Ekspertowe by Łukasz Ochmański "
 				+ "& Przemysław Szwajkowski");
 		stage.setScene(scene);
 		stage.sizeToScene();
 		stage.show();
-
-		translate.play();
-		rotate.play();
+		
+		playButton.setOnAction(new PlayButtonHandler());
+		pauseButton.setOnAction(new PauseButtonHandler());
+		stopButton.setOnAction(new StopButtonHandler());
+	}
+	
+	public static void main(String[] args) {
+		Application.launch();
 	}
 
-	public static void main(String[] args) {
-		Application.launch("blog.TheTranslateTransition");
+	class PlayButtonHandler implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent arg0) {
+			Plansza.this.translate.play();
+			Plansza.this.rotate.play();			
+		}
+	}
+	
+	class PauseButtonHandler implements EventHandler<ActionEvent> {
+		
+		@Override
+		public void handle(ActionEvent arg0) {
+			Plansza.this.translate.pause();
+			Plansza.this.rotate.pause();			
+		}
+	}
+	
+	class StopButtonHandler implements EventHandler<ActionEvent> {
+		
+		@Override
+		public void handle(ActionEvent arg0) {
+			Plansza.this.translate.stop();;
+			Plansza.this.rotate.stop();			
+		}
 	}
 }
