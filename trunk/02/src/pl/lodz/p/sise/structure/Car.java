8f640ed92c362461fb9230bf4d3d89da7fff0421
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-06-13 11:55:01 +0000

    Współrzędne policzone.

--- trunk/02/src/pl/lodz/p/sise/structure/Car.java
+++ trunk/02/src/pl/lodz/p/sise/structure/Car.java
@@ -4,6 +4,7 @@ import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
+import javafx.geometry.Point2D;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
@@ -22,17 +23,17 @@ public class Car extends Rectangle {
 		translate.setNode(this);
 		translate.setFromX(0);
 		translate.setFromY(0);
-		translate.setToX(400);
-		translate.setToY(200);
+		translate.setToX(710);
+		translate.setToY(0);
 		translate.setAutoReverse(true);
 		translate.setCycleCount(Timeline.INDEFINITE);
 		translate.setInterpolator(Interpolator.EASE_BOTH);
 		
 		rotate = new RotateTransition();
-		rotate.setDuration(new Duration(3 * 1000));
+		rotate.setDuration(new Duration(5 * 1000));
 		rotate.setNode(this);
 		rotate.setFromAngle(0);
-		rotate.setToAngle(180);
+		rotate.setToAngle(0);
 		rotate.setAutoReverse(true);
 		rotate.setCycleCount(Timeline.INDEFINITE);
 		rotate.setInterpolator(Interpolator.EASE_BOTH);
@@ -57,5 +58,34 @@ public class Car extends Rectangle {
 			return true;
 		else
 			return false;
+	}
+	
+	public Point2D getOffset(double x, double y) {
+//		final Point2D windowCoord = new Point2D(scene.getWindow().getX(), scene.getWindow().getY());
+//		final Point2D sceneCoord = new Point2D(scene.getX(), scene.getY());
+//		final Point2D paneCoord = new Point2D(pane.getLayoutX(), pane.getLayoutY());
+//		System.out.println("Do celu:\tx: "+clickX+"\ty: "+clickY);
+		
+		//double celX = scene.getWidth()-(rect02.getWidth()-10);
+		//double celY = scene.getHeight()/2-(rect02.getHeight()-10);
+		
+		Point2D nodeCoord = this.localToParent(this.getX(), this.getY());
+//		Point2D nodeCoord = new Point2D(Plansza.this.pojazd01.getX(), Plansza.this.pojazd01.getY());
+		
+//		Point2D parkingCoord = new Point2D(Plansza.this.rect02.getX(), Plansza.this.rect02.getY());
+		double offsetX = nodeCoord.getX() - x;
+		double offsetY = nodeCoord.getY() - y;
+		Point2D offset = new Point2D(offsetX, offsetY);
+		
+//		System.out.println("Parking:\tx: "+clickX+"\ty: "+clickY);
+//		System.out.println("Pause");
+//		System.out.println("Window " + windowCoord);
+//		System.out.println("Scene " + sceneCoord);
+//		System.out.println("Scene dimensions: " +scene.getWidth() +" "+ scene.getHeight());
+//		System.out.println("Pane " + paneCoord);
+//		System.out.println("Car " + nodeCoord);
+//		System.out.println("Parking " + parkingCoord);
+//		System.out.println("Missing " + offset);
+		return offset;
 	}
 }
