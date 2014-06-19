df034fb17018189a1f671329c8ec7a456a5b5c90
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-06-19 15:02:13 +0000

    Działający symulator parkowania.

--- 01/src/pl/lodz/p/sise/Heuristics.java
+++
@@ -1,5 +0,0 @@
-package pl.lodz.p.sise;
-
-public enum Heuristics {
-	None, ManhattanDistance, HammingDistance, SumOfManhattanDistances
-}
--- 01/src/pl/lodz/p/sise/Ruch.java
+++
@@ -1,8 +0,0 @@
-package pl.lodz.p.sise;
-
-public enum Ruch {
-	L, P, G, D
-//	D, G, P, L
-//	G, D, L, P
-//	P, L, D, G
-}
