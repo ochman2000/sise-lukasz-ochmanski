8e6c75c0dba3b99ce50b61de10bf2d72536e8faf
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-06-19 12:29:03 +0000

    refactoring kodu.

--- trunk/02/src/pl/lodz/p/sise/DrivingClass.java
+++ trunk/02/src/pl/lodz/p/sise/DrivingClass.java
@@ -1,35 +1,73 @@
 package pl.lodz.p.sise;
 
-import net.sourceforge.jFuzzyLogic.FIS;
-import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import java.util.List;
 
-public class DrivingClass {
-	public static void main(String[] args) throws Exception {
-		String filename = "fcl/driver.fcl";
-		FIS fis = FIS.load(filename, true);
+import pl.lodz.p.sise.structure.Car;
+import pl.lodz.p.sise.structure.Parking;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-		if (fis == null) {
-			System.err.println("Can't load file: '" + filename + "'");
-			System.exit(1);
-		}
+/**
+ * Simple example demonstrating JavaFX animations.
+ * 
+ * 2014-06-19, Warsaw, Poland
+ * 
+ * @author Łukasz Ochmański
+ */
+public class DrivingClass extends Application {
+	
+	private Pane pane;
+	private Scene scene;
+	private Car pojazd02;
 
-		// Get default function block
-		FunctionBlock fb = fis.getFunctionBlock(null);
+	
 
-		// Set inputs
-		fb.setVariable("food", 8.5);
-		fb.setVariable("service", 7.5);
+	/**
+	 * Start the JavaFX application
+	 * 
+	 * @param stage
+	 *            Primary stage.
+	 * @throws Exception
+	 *             Exception thrown during application.
+	 */
+	@Override
+	public void start(final Stage stage) throws Exception {
+		pane = new Parking();
 
-		// Evaluate
-		fb.evaluate();
+		scene = new Scene(pane, 900, 500);
+		scene.getStylesheets().addAll("file:resources/style.css");
 
-		// Show output variable's chart
-		fb.getVariable("zaparkowany").defuzzify();
+		stage.setTitle("Sztuczna Inteligencja i Systemy Ekspertowe by Łukasz Ochmański "
+				+ "& Przemysław Szwajkowski");
+		stage.setScene(scene);
+		stage.sizeToScene();
+		stage.show();
+		Animation animation = new Animation();
+		animation.setApplication(this);
+		animation.applyAnimation(pane);
+		
+	}
 
-		// Print ruleSet
-//		System.out.println(fb);
-		System.out.println("Samochód zaparkowany: " + fb.getVariable("zaparkowany").getName());
-		System.out.println(fb.getVariable("zaparkowany").getValue());
-
+	/**
+	 * Main function for running JavaFX application.
+	 * 
+	 * @param arguments
+	 *            Command-line arguments; optional first argument is the opacity
+	 *            of the path to be displayed (0 effectively renders path
+	 *            invisible).
+	 */
+	public static void main(final String[] arguments) {
+		Application.launch(arguments);
 	}
 }
--- trunk/02/src/pl/lodz/p/sise/structure/Parking.java
+++ trunk/02/src/pl/lodz/p/sise/structure/Parking.java
@@ -1,20 +1,9 @@
 package pl.lodz.p.sise.structure;
 
-import java.util.List;
-
-import javafx.animation.PathTransition;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.CubicCurveTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import javafx.util.Duration;
 
 /**
  * Simple example demonstrating JavaFX animations.
@@ -23,11 +12,8 @@ import javafx.util.Duration;
  * 
  * @author Łukasz Ochmański
  */
-public class Parking extends Application {
+public class Parking extends Pane {
 	private static final int SZEROKOSC_MIEJSCA_PARKINGOWEGO = 70;
-	private Pane pane;
-	private Scene scene;
-	private Car pojazd02;
 	private Rectangle line01;
 	private Rectangle line02;
 	private Rectangle line03;
@@ -38,99 +24,12 @@ public class Parking extends Application {
 	private ImageView pojazd01;
 	private ImageView pojazd03;
 	private ImageView pojazd04;
+	
+	public Parking() {
+		super();
+		this.setId("pane");
 
-	/**
-	 * Generate Path upon which animation will occur.
-	 * 
-	 * @param pathOpacity
-	 *            The opacity of the path representation.
-	 * @return Generated path.
-	 */
-	private Path generateCurvyPath(final double pathOpacity) {
-		final Path path = new Path();
-		path.getElements().add(new MoveTo(100, 200));
-		path.getElements().add(new CubicCurveTo(100, 200, 200, 300, 300, 200));
-		path.getElements().add(new CubicCurveTo(300, 200, 400, 100, 500, 200));
-		path.setOpacity(pathOpacity);
-		return path;
-	}
-
-	/**
-	 * Generate the path transition.
-	 * 
-	 * @param shape
-	 *            Shape to travel along path.
-	 * @param path
-	 *            Path to be traveled upon.
-	 * @return PathTransition.
-	 */
-	private PathTransition generatePathTransition(final ImageView shape,
-			final Path path) {
-		final PathTransition pathTransition = new PathTransition();
-		pathTransition.setDuration(Duration.seconds(8.0));
-		pathTransition.setDelay(Duration.seconds(2.0));
-		pathTransition.setPath(path);
-		pathTransition.setNode(shape);
-		pathTransition
-				.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-		pathTransition.setCycleCount(Timeline.INDEFINITE);
-		pathTransition.setAutoReverse(true);
-		return pathTransition;
-	}
-
-	/**
-	 * Determine the path's opacity based on command-line argument if supplied
-	 * or zero by default if no numeric value provided.
-	 * 
-	 * @return Opacity to use for path.
-	 */
-	private double determinePathOpacity() {
-		final Parameters params = getParameters();
-		final List<String> parameters = params.getRaw();
-		double pathOpacity = 1.0;
-		if (!parameters.isEmpty()) {
-			try {
-				pathOpacity = Double.valueOf(parameters.get(0));
-			} catch (NumberFormatException nfe) {
-				pathOpacity = 0.0;
-			}
-		}
-		return pathOpacity;
-	}
-
-	/**
-	 * Apply animation, the subject of this class.
-	 * 
-	 * @param group
-	 *            Group to which animation is applied.
-	 */
-	private void applyAnimation(final Pane group) {
-		pojazd02 = new Car();
-		final Path path = generateCurvyPath(determinePathOpacity());
-		group.getChildren().add(path);
-		group.getChildren().add(pojazd02);
-		final PathTransition transition = generatePathTransition(pojazd02, path);
-		transition.play();
-	}
-
-	/**
-	 * Start the JavaFX application
-	 * 
-	 * @param stage
-	 *            Primary stage.
-	 * @throws Exception
-	 *             Exception thrown during application.
-	 */
-	@Override
-	public void start(final Stage stage) throws Exception {
-		pane = new Pane();
-		pane.setId("pane");
-
-		scene = new Scene(pane, 900, 500);
-		scene.getStylesheets().addAll(
-				this.getClass().getResource("style.css").toExternalForm());
-
-		Image image = new Image(this.getClass().getResource("car01.png").toExternalForm());
+		Image image = new Image("file:resources/car01.png");
 		
 		line00 = new Rectangle(120, 1);
 		line00.setId("linia00");
@@ -188,26 +87,7 @@ public class Parking extends Application {
 		line05.setX(700);
 		line05.setY(215+3*SZEROKOSC_MIEJSCA_PARKINGOWEGO);
 		
-		pane.getChildren().addAll(line00, line01, line02, line03, line04, line05,
+		this.getChildren().addAll(line00, line01, line02, line03, line04, line05,
 				pojazd00, pojazd01, pojazd03, pojazd04);
-
-		stage.setTitle("Sztuczna Inteligencja i Systemy Ekspertowe by Łukasz Ochmański "
-				+ "& Przemysław Szwajkowski");
-		stage.setScene(scene);
-		stage.sizeToScene();
-		stage.show();
-		applyAnimation(pane);
-	}
-
-	/**
-	 * Main function for running JavaFX application.
-	 * 
-	 * @param arguments
-	 *            Command-line arguments; optional first argument is the opacity
-	 *            of the path to be displayed (0 effectively renders path
-	 *            invisible).
-	 */
-	public static void main(final String[] arguments) {
-		Application.launch(arguments);
 	}
 }
