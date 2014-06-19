b6d66db6f93e1aea8528de3a1626701921805472
ochman2000@gmail.com <ochman2000@gmail.com@a413b68c-17fc-9e4b-632d-c91942c1e04d>
2014-06-19 14:04:53 +0000

    Dodane menu.

---
+++ trunk/02/src/pl/lodz/p/sise/handlers/PlayButtonHandler.java
@@ -0,0 +1,14 @@
+package pl.lodz.p.sise.handlers;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class PlayButtonHandler implements EventHandler<ActionEvent> {
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
